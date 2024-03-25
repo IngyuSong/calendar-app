db59321e2c707fbbfaac41cb1b2825a144eaf489
Ingyu Song <ingyu3879@gmail.com>
2024-03-26 00:20:18 +0900

    :sparkles: CSV 파일로 data 대량 등록 처리

--- src/main/java/org/example/calendarapp/CalendarAppApplication.java
+++ src/main/java/org/example/calendarapp/CalendarAppApplication.java
@@ -1,48 +1,27 @@
 package org.example.calendarapp;
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.example.calendarapp.event.AbstractEvent;
-import org.example.calendarapp.event.Event;
-import org.example.calendarapp.event.EventType;
 import org.example.calendarapp.event.Meeting;
 import org.example.calendarapp.event.Schedule;
-import org.example.calendarapp.event.Todo;
+import org.example.calendarapp.reader.EventCsvReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalendarAppApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     SpringApplication.run(CalendarAppApplication.class, args);
 
     Schedule schedule = new Schedule();
 
-    HashSet<String> participants = new HashSet<>();
-    participants.add("danny.kim");
+    EventCsvReader csvReader = new EventCsvReader();
+    String meetingCsvPath = "/data/meeting.csv";
 
-    Meeting meeting1 = new Meeting(
-        1, "meeting1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1), participants, "meetingRoomA", "스터디"
-    );
-    schedule.add(meeting1);
-
-    Todo todo1 = new Todo(
-        2, "todo1", ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4), "할 일 적기"
-    );
-    schedule.add(todo1);
-
-    Todo todo2 = new Todo(
-        3, "todo2", ZonedDateTime.now().plusHours(5), ZonedDateTime.now().plusHours(4), "할 일 적기"
-    );
-    schedule.add(todo2);
+    List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+    meetings.forEach(schedule::add);
 
     schedule.printAll();
-
-
   }
-
 }
